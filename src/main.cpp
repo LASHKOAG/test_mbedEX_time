8e6f9dd9ca09c03608913f268b9e0ae932a08c1b
lashkoag <lashko-ag@energos.su>
2020-03-06 12:38:51 +0600

    intermediate work with function

--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,38 @@
-/*
-743 test_mbedEX_time
-*/
-
-/* mbed Example Program
- * Copyright (c) 2006-2014 ARM Limited
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Example to setup the Real-Time Clock from a terminal, sford
+ 
 #include "mbed.h"
-#include <iostream>
 
-int main ()
-{
-  time_t rawtime;
- 
-  time ( &rawtime ); // текущая дата в секундах
-  std::cout << "Текущее локальное время и дата: "
-            <<  ctime (&rawtime); // текущая дата в понятной для человека форме
- 
-  return 0;
+void write_rtc(){
+    //printf("YYYY MM DD HH MM SS[enter]\n");
+    struct tm t;
+    
+    t.tm_year=2020;
+    t.tm_mon=03;
+    t.tm_mday=06;
+    t.tm_hour=11;
+    t.tm_min=47;
+    t.tm_sec=23;
+
+    t.tm_year = t.tm_year - 1900;
+    t.tm_mon = t.tm_mon - 1;
+
+    // set the time
+    set_time(mktime(&t));
 }
-/*
+
+void read_rtc(){
+    time_t seconds = time(NULL);
+    printf("Time as a basic string = %s", ctime(&seconds));
+}
+
 int main() {
-    set_time(1256729737);  // Set RTC time to Wed, 28 Oct 2009 11:35:37
-
-    while (true) {
-        time_t seconds = time(NULL);
-        
-        printf("Time as seconds since January 1, 1970 = %u\n", (unsigned int)seconds);
-        
-        printf("Time as a basic string = %s", ctime(&seconds));
-
-        char buffer[32];
-        strftime(buffer, 32, "%I:%M %p\n", localtime(&seconds));
-        printf("Time as a custom formatted string = %s", buffer);
-        
+    write_rtc();
+        // display the time
+    while(1) {    
+        read_rtc();
         wait(1);
     }
 }
-*/
+
+
+ //scanf("%d %d %d %d %d %d", &t.tm_year, &t.tm_mon, &t.tm_mday, &t.tm_hour, &t.tm_min, &t.tm_sec);
