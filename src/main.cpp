ad9324ec4da7ff59305089963ea91ca591125ada
lashkoag <lashko-ag@energos.su>
2020-03-06 13:21:32 +0600

    intermediate work with function

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 // Example to setup the Real-Time Clock from a terminal, sford
  
 #include "mbed.h"
-
+#include <string>
+#include <iostream>
 void write_rtc(){
     //printf("YYYY MM DD HH MM SS[enter]\n");
     struct tm t;
@@ -18,18 +19,45 @@ void write_rtc(){
 
     // set the time
     set_time(mktime(&t));
+    /*
+        std::string input = "2020.03.06 12.13.32";
+    std::string firstWord = input.substr(0, input.find("."));
+        std::cout << " The word is: " << firstWord << std::endl;
+    int getData= std::stoi(firstWord);
+    */
 }
 
-void read_rtc(){
+void read_rtc(std::string* input){
     time_t seconds = time(NULL);
     printf("Time as a basic string = %s", ctime(&seconds));
+    //std::string input = "hello arm";
+    //std::string input = "2020.03.06 12.13.32";
+    std::string firstWord = input.substr(0, input.find("."));
+        std::cout << " The word is: " << firstWord << std::endl;
+    int getData= std::stoi(firstWord);
+    firstWord = input.substr(5, 2);
+        std::cout << " The word is: " << firstWord << std::endl;
+    
+    firstWord = input.substr(8, 2);
+        std::cout << " The word is: " << firstWord << std::endl;
+    firstWord = input.substr(11, 2);
+        std::cout << " The word is: " << firstWord << std::endl;
+    firstWord = input.substr(14, 2);
+        std::cout << " The word is: " << firstWord << std::endl;
+    firstWord = input.substr(17, 2);
+        std::cout << " The word is: " << firstWord << std::endl;
+        std::cout << " ===================== " << std::endl;
+        printf("getData = %d\n", getData);
+
 }
 
 int main() {
     write_rtc();
         // display the time
+        char text[100]="2020.03.06 12.13.32";
+        std::string text2="2020.03.06 12.13.32";
     while(1) {    
-        read_rtc();
+        read_rtc(text);
         wait(1);
     }
 }
